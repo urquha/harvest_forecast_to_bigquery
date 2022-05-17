4a44d4fff0819712cd5cb31927f35e151b8427a2
urquha <andy.urquhart@hotmail.co.uk>
2022-05-17 16:47:03 +0100

    Transformed code into cloud function

--- get_forcast_data.py
+++ get_forcast_data.py
@@ -4,7 +4,6 @@ import os
 import pytest
 from data_response_key_lists import person_data_keys
 import json
-import sys
 import io
 
 from google.cloud import bigquery
@@ -28,24 +27,23 @@ def json_list(response: list):
     return list(map(lambda item: item._json_data, response)) 
 
 def load_to_bigquery():
-    
-    table_id = table_names[0]
-    dataset_ref = client.dataset(dataset_id)
-    table_ref = dataset_ref.table(table_id)
-    job_config = bigquery.LoadJobConfig()
-    job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-    job_config.autodetect = True
+    for table_name, data in tables.items():
+        dataset_ref = client.dataset(dataset_id)
+        table_ref = dataset_ref.table(table_name)
+        job_config = bigquery.LoadJobConfig()
+        job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+        job_config.autodetect = True
+        
+        print("Attempting to load {} rows into {}:{}.".format(len(data), dataset_id, table_name))
 
-    job = client.load_table_from_json(
-        assignments, 
-        table_ref,
-        location="europe-west2", 
-        job_config=job_config,
-        num_retries=0,
-        timeout=2)
-    print(job.result())
+        job = client.load_table_from_file(
+            io.StringIO('\n'.join(data)), 
+            table_ref,
+            location="europe-west2", 
+            job_config=job_config,
+            )
+        job.done()
 
-    print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
 
 
 if __name__ == "__main__":
@@ -53,12 +51,12 @@ if __name__ == "__main__":
         account_id=os.environ.get("HARVEST_ACCOUNT_ID"), 
         auth_token=os.environ.get("HARVEST_ACCESS_TOKEN")
     )
-    assignments = get_assignments_data()
-    # pytest.set_trace()
-    clients = '\n'.join(get_clients_data())
-    projects = '\n'.join(get_projects_data())
+    tables = {
+        'assignments_data': get_assignments_data(),
+        'projects_data': get_clients_data(), 
+        'clients_data': get_projects_data()
+    }
     dataset_id = 'forecast_001'
-    table_names = ['assignments_data', 'projects_data', 'clients_data']
 
     client = bigquery.Client()
     load_to_bigquery()
--- main.py
+++ main.py
@@ -1,49 +1,62 @@
 import forecast
 from dotenv import load_dotenv
 import os
-import pytest
-from data_response_key_lists import person_data_keys
+import json
+import io
 
-load_dotenv()
+from google.cloud import bigquery
 
-def get_forecast_data():
+
+def get_assignments_data(api):
+    assignments = json_list(api.get_assignments(start_date="2022-04-01T00:00:00.000Z", end_date="2022-05-01T00:00:00.000Z"))
+    return [json.dumps(record) for record in assignments]
+
+def get_clients_data(api):
+    clients = json_list(api.get_clients())
+    return [json.dumps(record) for record in clients]
+
+def get_projects_data(api):
+    projects = json_list(api.get_projects())
+    return [json.dumps(record) for record in projects]
+
+def json_list(response: list):
+    # This function maps a response to a json list
+    return list(map(lambda item: item._json_data, response)) 
+
+def forecast_to_bigquery(response):
+    load_dotenv()
     api = forecast.Api(
         account_id=os.environ.get("HARVEST_ACCOUNT_ID"), 
         auth_token=os.environ.get("HARVEST_ACCESS_TOKEN")
     )
-         
-    # projects = api.get_projects()
-    # projects_json = get_json_data(projects)
+    tables = {
+        'assignments_data': get_assignments_data(api),
+        'projects_data': get_clients_data(api), 
+        'clients_data': get_projects_data(api)
+    }
+    dataset_id = 'forecast_001'
 
-    # people = api.get_people()
-    # people_json = get_json_data(people)
-    
-    # clients = api.get_clients()
-    # clients_json = get_json_data(clients)
+    client = bigquery.Client()
+    response = {"code": 200, "result": []}
+    for table_name, data in tables.items():
+        dataset_ref = client.dataset(dataset_id)
+        table_ref = dataset_ref.table(table_name)
+        job_config = bigquery.LoadJobConfig()
+        job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+        job_config.autodetect = True
 
-    # milestones = api.get_milestones()
-    # milestones_json = get_json_data(milestones)
-
-    assignments = api.get_assignments(start_date="2022-04-06T14:40:50.925Z", end_date="2022-04-25T14:40:50.925Z", person_id=109200)
-    assignments_json = get_json_data(assignments)
-
-    return assignments_json
-
-
-
+        response['result'].append({'table_name': f"{len(data)} rows"})
         
-def get_json_data(response_list):
-    json_return = {}
-    for item in [response_list[0]]:
-        json_return[item.id] = item._json_data
-    return json_return
+        print("Attempting to load {} rows into {}:{}.".format(len(data), dataset_id, table_name))
 
-
-def print_list(lst: list):
-    for item in lst:
-        print(item)
-
-if __name__ == "__main__":
+        job = client.load_table_from_file(
+            io.StringIO('\n'.join(data)), 
+            table_ref,
+            location="europe-west2", 
+            job_config=job_config,
+            )
+        job.done()
     
-    # This is the data we want to put into bigquery
-    forecast_data = get_forecast_data()
+    return response
+
+    
