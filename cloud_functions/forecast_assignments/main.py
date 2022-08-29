46b9a7c837e08eac51b7a9440194fb4abab75df2
urquha <andy.urquhart@hotmail.co.uk>
2022-08-29 23:02:59 +0100

    flattened assignment dates

--- cloud_functions/forecast_assignments/main.py
+++ cloud_functions/forecast_assignments/main.py
@@ -4,6 +4,7 @@ import json
 from google.cloud.logging import DESCENDING
 from datetime import datetime, timedelta, timezone
 import pandas as pd
+import copy
 
 from google.cloud import bigquery
 
@@ -49,10 +50,9 @@ def forecast_assignments_to_bigquery(data: dict, context:dict=None):
     )
 
     print(f"Getting assignments data from {start_timestamp} to {end_timestamp}")
-    assignments_data_df = pd.DataFrame(get_assignments_data(api, start_timestamp, end_timestamp)).drop(['placeholder_id'], axis=1)
-
+    assignments_data_df = expand_rows(pd.DataFrame(get_assignments_data(api, start_timestamp, end_timestamp)).drop(['placeholder_id'], axis=1))
+    
     client = bigquery.Client(location=config['location'])
-
     dataset_ref = client.dataset(config['dataset_id'])
     table_ref = dataset_ref.table(config['table_name'])
     job_config = bigquery.LoadJobConfig()
@@ -66,6 +66,32 @@ def forecast_assignments_to_bigquery(data: dict, context:dict=None):
 
     print("Loaded {} rows into {}:{}.".format(job.output_rows, config['dataset_id'], config['table_name']))    
 
+def expand_rows(df):
+    # When an assignment is entered, it can be put in for a single day or multiple. 
+    # For entries spanning across multiple days, this function converts to single day entries and returns the dataframe.
+    rows_to_edit = []
+    single_assignment_rows = []
+    for index, row in df.iterrows():
+        if row['start_date'] != row['end_date']:
+            rows_to_edit.append(row)
+        else:
+            single_assignment_rows.append(row)
+
+    for row in rows_to_edit:
+        end_date = datetime.strptime(row['end_date'], '%Y-%m-%d')
+        start_date = datetime.strptime(row['start_date'], '%Y-%m-%d')
+        date = start_date
+
+        while date <= end_date:
+            if date.weekday() > 4:
+                date = date + timedelta(days=(7 - date.weekday()))
+            string_date = datetime.strftime(date, '%Y-%m-%d')
+            row['start_date'] = string_date
+            row['end_date'] = string_date
+            single_assignment_rows.append(copy.copy(row))
+            date = date + timedelta(days=1)
+
+    return pd.DataFrame(single_assignment_rows)
 
 if __name__ == "__main__":
     forecast_assignments_to_bigquery({}, {})
